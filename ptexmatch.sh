2a576fbf51969a1063ffdb3946186c589fd9454c
Linus Bergman <linusbergman@pm.me>
2021-04-19 19:56:45 +0300

    Initial commit

---
+++ ptexmatch.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Utility to check which Audio Network files are used in Pro Tools .ptx file(s).
+# Exports a csv-like file with Pro Tools session name in field one followed by
+# n number of fields of music files.
+#
+# written by Linus Bergman
+#
+# The script requires ptftool by Damien Zammit @zamaudio to be in the same folder
+# as this utility.
+# See https://github.com/zamaudio/ptformat
+
+
+TMPFILE=$(mktemp /tmp/ANW-script.XXXX)
+
+read -e -p 'Folder to scan: ' ROOTFOLDER
+read -p 'Output to file: ' OUTPUT
+
+{
+		find "$ROOTFOLDER" -type f -name '*.ptx' | grep -v Backups | egrep -e '-PG|-PR' |
+		while read i; do
+				echo Traversing "$i"
+				printf "$i" | awk -F'/' '{print $NF}' | sed 's/.ptx//' >> "$TMPFILE"
+
+				./ptftool "$i"  | grep -i anw | 
+												sed 's/\..*//' | 
+												sed 's/\`//' | 
+												sed 's/\`.*//' | 
+												sed -E 's/-[0-9]+$//' |
+												sed -e 's/\(ANW.*\).*/ \1/' |
+												sort -u >> "$TMPFILE"
+		done
+}
+
+cat $TMPFILE | sed 'H;1h;$!d;g;s/\n  */,/g' >> "$OUTPUT"
+rm $TMPFILE
+
+		
+
